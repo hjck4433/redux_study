2e737ef2c9072b73a3079e129913a4c2fc187e7c
hjck4433 <hjck4433@gmail.com>
2024-05-06 21:48:24 +0900

    Todo list 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./reducers";
 
 type Props = {
-  value: number;
+  value: any;
   onIncrement: () => void;
   onDecrement: () => void;
 };
 
 function App({ value, onIncrement, onDecrement }: Props) {
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+
+  const dispatch = useDispatch();
+
+  const [todoValue, setTodoValue] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", text: todoValue });
+    setTodoValue("");
+  };
   return (
     <div className="App">
-      Clicked: {value} times
+      Clicked: {counter} times
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,33 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import counter from "./reducers";
 import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const store = createStore(counter);
+const store = createStore(rootReducer);
+
+store.dispatch({
+  type: "ADD_TODO",
+  text: "Use Redux",
+});
+
+console.log("store.getState()", store.getState());
 
 const render = () =>
   root.render(
     <React.StrictMode>
-      <App
-        value={store.getState()}
-        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-      />
+      <Provider store={store}>
+        <App
+          value={store.getState()}
+          onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+          onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+        />
+      </Provider>
     </React.StrictMode>
   );
 
